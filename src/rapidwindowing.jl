38654ebf2559ea6b9da063335ab854c3ff2bcf48
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-08-03 13:21:31 -0400

    Update rapidwindowing.jl

--- src/rapidwindowing.jl
+++ src/rapidwindowing.jl
@@ -5,12 +5,18 @@ const EightInts  = Tuple{Int, Int, Int, Int, Int, Int, Int, Int}
 
 const CurrentWindowInfo = NamedTuple{EightNames, EightInts}
 
-function updatewindowinfo(datasrc::WindowedData, windowedfunc::WindowedFunction, wi::CurrentWindowInfo)
-  newstart, newstop = wi.start + 1, wi.stop + 1
-  newstop <= datasrc.srcspan && newstart > 0 || DomainError(datasrc.srcspan,  newstart, newstop)
-  oldestval = datasrc.source[newstart]
-  newestval = datasrc.source[newstop]
-  updatefrom, upsatewith = windowedfunc.update(datasrc, newstart, newstop, wi.updatefrom, wi.updatewith)
-  CurrentWindowInfo((wi.windowspan, newstart, newstop, oldestval, newestval, updatefrom, updatewith)
-end 
+function advance_window(datasrc::WindowedData, currentwindow::CurrentWindowInfo)
+    newstart, newstop = wi.start + 1, wi.stop + 1
+    newstop <= datasrc.srcspan && newstart > 0 || DomainError(datasrc.srcspan,  newstart, newstop)
+    newstart, newstop
+end  
+
+function update_window(datasrc::WindowedData, currentwindow::CurrentWindowInfo, windowedfunc::WindowedFunction)
+    newstart, newstop = advance_window(datasrc, currentwindow)
+    oldestval = datasrc.source[newstart]
+    newestval = datasrc.source[newstop]
+    updatefrom, upsatewith = windowedfunc.update(datasrc, newstart, newstop, 
+                                                 currentwindow.updatefrom, currentwindow.updatewith)
+    CurrentWindowInfo((datasrc.windowspan, newstart, newstop, oldestval, newestval, updatefrom, updatewith)
+end
   
