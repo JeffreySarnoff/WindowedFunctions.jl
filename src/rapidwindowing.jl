5f3ba2647b34398046a0a1809e61d53563aa33a8
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-08-03 12:36:26 -0400

    Update rapidwindowing.jl

--- src/rapidwindowing.jl
+++ src/rapidwindowing.jl
@@ -1,2 +1,16 @@
 # rapidwindowing
 
+const EightNames = (:windowspan, :start, :stop, :oldestval, :newestval, :updatefrom, :updatewith)
+const EightInts  = Tuple{Int, Int, Int, Int, Int, Int, Int, Int}
+
+const CurrentWindowInfo = NamedTuple{EightNames, EightInts}
+
+function updatewindowinfo(datasrc::WindowedData, windowedfunc::WindowedFunction, wi::CurrentWindowInfo)
+  newstart, newstop = wi.start + 1, wi.stop + 1
+  newstop <= datasrc.srcspan && newstart > 0 || DomainError(datasrc.srcspan,  newstart, newstop)
+  oldestval = datasrc.source[newstart]
+  newestval = datasrc.source[newstop]
+  updatefrom, upsatewith = windowedfunc.update(datasrc, newstart, newstop, wi.updatefrom, wi.updatewith)
+  CurrentWindowInfo((wi.windowspan, newstart, newstop, oldestval, newestval, updatefrom, updatewith)
+end 
+  
