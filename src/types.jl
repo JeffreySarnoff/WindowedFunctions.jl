977a6d050d9eebd2e725f4be5326f6e0fee504e2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-08-03 11:52:59 -0400

    Update types.jl

--- src/types.jl
+++ src/types.jl
@@ -2,9 +2,9 @@
 
 abstract type AbstractWindowed end
 abstract type AbstractWindowedFunction <: AbstractWindowed end
-abstract type AbstractWindowedData     <: AbstractWindowed end
+abstract type AbstractWindowedData{T}  <: AbstractWindowed end
 
-abstract type AbstractWindowedArray{Ndims,T}  <: AbstractWindowedData end
+abstract type AbstractWindowedArray{Ndims,T}  <: AbstractWindowedData{T} end
 abstract type AbstractWindowedVector{T}       <: AbstractWindowedArray{1,T} end
 abstract type AbstractWindowedTwoVectors{T}   <: AbstractWindowedArray{2,T} end
 abstract type AbstractWindowedThreeVectors{T} <: AbstractWindowedArray{3,T} end
@@ -17,6 +17,21 @@ abstract type AbstractBinaryOperator     <: AbstractWindowedOperator{2} end
 abstract type AbstractTrinaryOperator    <: AbstractWindowedOperator{3} end
 abstract type AbstractQuaternaryOperator <: AbstractWindowedOperator{4} end
 
+struct WindowedFunction{Setup, Apply<:Function, Update<:Function, Tracking, Arity}
+    setup::Setup
+    apply::Apply
+    update::Update
+    tracking::Tracking
+end
+
+struct WindowedData{T,Nseqs,Eltype,} <: AbstractWindowedData{T}
+     source::T
+     sourcespan::Int
+     windowspan::Int
+     ndetermined::Int     # how many values are fully determined, n_full_values <= sourcespan
+     ntrailing::Int       # how many values are partially windowed, n_partial_values <= mod(sourcespan, windowspan)
+end
 
 
-struct DataWindow <: AbstractDataWindow
+     
+     
