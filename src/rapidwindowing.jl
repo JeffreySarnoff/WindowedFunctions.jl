a30f61c5872a335719289ff9eb9cf78b2fbb25a5
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-08-04 08:20:51 -0400

    namedtuple

--- src/rapidwindowing.jl
+++ src/rapidwindowing.jl
@@ -1,22 +1,20 @@
 # rapidwindowing
 
-const EightNames = (:windowspan, :start, :stop, :oldestval, :newestval, :updatefrom, :updatewith)
-const EightInts  = Tuple{Int, Int, Int, Int, Int, Int, Int, Int}
+const WindowInfo = 
+    NamedTuple{(:span, :firstidx, :finalidx, :firstval, :finalval), NTuple{5,Int}}
 
-const CurrentWindowInfo = NamedTuple{EightNames, EightInts}
-
-function advance_window(datasrc::WindowedData, currentwindow::CurrentWindowInfo)
+function advance_window(datasrc::WindowedData, currentwindow::WindowInfo)
     newstart, newstop = wi.start + 1, wi.stop + 1
     ((newstop <= datasrc.srcspan) && (newstart > 0)) || 
         WindowBoundsErr((sourcelen=datasrc.srcspan,  start=newstart, stop=newstop))
     newstart, newstop
 end  
 
-function update_window(datasrc::WindowedData, currentwindow::CurrentWindowInfo, windowedfunc::WindowedFunction)
+function update_window(datasrc::WindowedData, currentwindow::WindowInfo, windowedfunc::WindowedFunction)
     newstart, newstop = advance_window(datasrc, currentwindow)
     oldestval = datasrc.source[newstart]
     newestval = datasrc.source[newstop]
-    updatefrom, upsatewith = windowedfunc.update(datasrc, newstart, newstop, 
+    updatefrom, updatewith = windowedfunc.update(datasrc, newstart, newstop, 
                                                  currentwindow.updatefrom, currentwindow.updatewith)
     CurrentWindowInfo((datasrc.windowspan, newstart, newstop, oldestval, newestval, updatefrom, updatewith)
 end
