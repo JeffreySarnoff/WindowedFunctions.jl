7166496702be8fbc328759ae92ca1549364278c8
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-12-06 01:18:24 -0500

    Update support.jl

--- src/support.jl
+++ src/support.jl
@@ -1,1 +1,34 @@
+# module specific exceptions
 
+struct SpanException <: Exception
+     msg::String
+end
+
+SpanError(sequence_length, window_span) =
+    throw(SpanException("The window span $(window_span) exceeds the sequence length $(sequence_length).")))
+
+ValidateSpan(sequence_length, window_span) =
+    (sequence_length >= window_span) || SpanError(sequence_length, window_span)
+
+struct NWeightsException <: Exception
+     msg::String
+end
+
+NWeightsError(nweights, window_span) =
+    throw(NWeightsException("The window span is not equal to the number of weights ($(window_span) != $(nweights))."))
+
+ValidateWeights(nweights::Int, window_span) =
+    (nweights == window_span) || NWeightsError(nweights, window_span)
+
+struct NormalizedWeightsException <: Exception
+     msg::String
+end
+
+NormalizedWeightsError(weights) =
+    throw(NormalizedWeightsException("The weights do not sum to ~1 (sum = $(sum(weights)))."))
+
+ValidateNormalizedWeights(weights) =
+    (isapprox(sum(weights), 1.0; rtol=eps(eltype(weights))) || NormalizedWeightsError(weights))
+
+ValidateWeights(weights, window_span) =
+    (ValidateWeights(length(weights), window_span); ValidateNormalizedWeights(weights)) 
