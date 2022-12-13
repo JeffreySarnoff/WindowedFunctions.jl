f21bbbd51a520350127bb9f123e5940beba13cdc
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-12-13 03:11:49 -0500

    Update WindowedFunctions.jl

--- src/WindowedFunctions.jl
+++ src/WindowedFunctions.jl
@@ -10,26 +10,43 @@ abstract type Gather{T} <: Function end
 abstract type Accumulator{T} <: Gather{T} end
 abstract type Aggregator{T}  <: Gather{T} end
 
-struct AccFn{T,A<:Accumulator,F<:Function} <: Accumulator{T}
+struct AccFx{T,A<:Accumulator,F<:Function} <: Accumulator{T}
     acc::A
-	fn::F
+    fn::F
 end
 
- function AccFn(acc::A, fn::Function) where{T,A<:Accumulator{T}}
-     AccFn{T,A,typeof(fn)}(acc, fn)
- end
+function AccFx(acc::A, fn::Function) where{T,A<:Accumulator{T}}
+    AccFn{T,A,typeof(fn)}(acc, fn)
+end
 	   
-function (acc::AccFunc)()
+function (acc::AccFx)()
     acc.acc()
 end
 
-function (acc::AccFunc)(x)
+function (acc::AccFx)(x)
     acc.acc(acc.fn(x))
 end
 
 
+struct AccumData{T}
+    acc::Accumulator{T}
+    data::Seq{T}
+end
+
+function Base.iterate(accumdata::AccumData{T}) where {T}
+     datastate = (first(accumdata.data), ifelse(length(accumdata.data) > 1, 2, nothing))
+     accstate  = (accumdata.acc(datastate[1]), 2)
+     (accumdata, datastate)
+end
+function Base.iterate(accumdata::AccumData{T}, state) where {T}
+     datastate = ifelse(isnothing(state), nothing, accumdata.data[state])
+     state = ifelse(length(accumdata.data) > state, state+1, nothing)
+     ifelse(!isnothing(datastate), accumdata.acc(datastate), nothing)
+     (accumdata, state)
+end
+
   
-struct AggFn{T, A<:Aggregator{T}, F<:Function} <: Aggregator{T}
+struct AggFx{T, A<:Aggregator{T}, F<:Function} <: Aggregator{T}
     aagg::A
     fn::F
 end
