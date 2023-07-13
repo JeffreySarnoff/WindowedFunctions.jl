06b65c1dba92a95d7b6c9db582951bfb75adc063
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-07-13 06:30:57 -0400

    Create runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,31 @@
+using RollingFunctions
+import RollingFunctions as RF
+using LinearAlgebra
+using StatsBase
+using VectorizedStatistics
+using Test
+
+clean(x) = x
+clean(x::Missing) = Missing
+
+include("wholesparts.jl")
+
+include("roll/rolling.jl")
+include("roll/roll_vectors.jl")
+include("roll/roll_weighted_vectors.jl")
+include("roll/roll_matrix.jl")
+include("roll/roll_stats.jl")
+
+include("tile/tiling.jl")
+include("tile/tile_vectors.jl")
+include("tile/tile_weighted_vectors.jl")
+include("tile/tile_matrix.jl")
+include("tile/tile_stats.jl")
+
+include("run/running.jl")
+include("run/run_vectors.jl")
+include("run/run_weighted_vectors.jl")
+include("run/run_matrix.jl")
+include("run/run_stats.jl")
+
+# include("deprecated.jl")
